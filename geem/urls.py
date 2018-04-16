0f1df7883496faacff051349901f5e7c817d0744
Dan Fornika <dfornika@gmail.com>
2018-04-15 20:42:06 -0700

    Wire up remaining pages to django views

--- geem/urls.py
+++ geem/urls.py
@@ -4,5 +4,12 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index.html', views.index),
+    path('portal.html', views.portal),
+    path('form.html', views.form),
+    path('data/shared_packages/test.epi.json', views.test_epi),
+    path('data/ontology/foodon-merged.json', views.foodon_merged),
+    path('data/ontology/genepio-merged.json', views.genepio_merged),
+    path('templates/modal_lookup.html', views.modal_lookup),
+    path('templates/resource_summary_form.html', views.resource_summary_form),
 ]
--- geem/views.py
+++ geem/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'geem/index.html', context={})
+
+def portal(request):
+    return render(request, 'geem/portal.html', context={})
+
+def form(request):
+    return render(request, 'geem/form.html', context={})
+
+def modal_lookup(request):
+    return render(request, 'geem/templates/modal_lookup.html', context={})
+
+def resource_summary_form(request):
+    return render(request, 'geem/templates/resource_summary_form.html', context={})
+
+def test_epi(request):
+    data = json.load(open('geem/static/geem/data/shared_packages/test.epi.json'))
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def genepio_merged(request):
+    data = json.load(open('geem/static/geem/data/ontology/genepio-merged.json'))
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def foodon_merged(request):
+    data = json.load(open('geem/static/geem/data/ontology/foodon-merged.json'))
+    return HttpResponse(json.dumps(data), content_type='application/json')

7b84636dfb41d65c069a5f8fda6f8c2030012e60
Damion Dooley <damion.dooley@bccdc.ca>
2018-07-18 22:06:09 -0700

    revised render_resource_form()

--- geem/static/geem/js/geem_resource.js
+++ geem/static/geem/js/geem_resource.js
@@ -1,83 +1,178 @@
+/* contains functions needed for portal.html resource selection and reporting section */
 
-function check_for_hash_entity() {
+function render_resource_form() {
+	// Feeds specification.metadata variables to copy of template
 
- if (location.hash.length > 0 && location.hash.indexOf(':') != -1) { 
-    top.focusEntityId = document.location.hash.substr(1).split('/',1)[0]
+	$.ajax('templates/resource_summary_form.html').done(function(response) {
 
-    // Returns if loading resource or if no appropriate resource found
-    if (!check_entity_resource(top.focusEntityId) ) return
+		const metadata = top.resource.metadata
+		$('#resourceForm').html( do_template_fill(response, metadata) )
 
-    // load_standalone_form()
-	// render_portal_form()
-  }
+		// 2 select lists to select options in:
+		$('#summary_type option[value="' + metadata.type + '"]').prop('selected', true)
+		$('#summary_status option[value="' + metadata.status + '"]').prop('selected', true)
+
+		// If loaded data is direct from ontology, hide certain buttons. 
+		var onto_fields = $('#summary_title, #summary_resource, #summary_description, #summary_prefix')
+
+		if (metadata.type != 'ontology')
+			$('.summary_prefix').hide()
+
+		if (metadata.type == 'private') {
+
+			$("#summary_license").removeAttr('readonly')
+
+			if (metadata.new) {
+				// new records are always private, draft packages.
+				$('.summary_resource, .summary_prefix').hide()
+				$('#summary_delete, #summary_download, #summary_copy').hide()
+			}
+			else //
+				$("#summary_status, #summary_type").prop('disabled', false)
+			
+		}
+		else {
+			// FUTURE: User may own a shared package, therefore can edit.
+			$('#summary_delete, #summary_update').hide()
+			
+			onto_fields.attr('readonly','readonly')
+
+		}
+
+		$('#resourceForm').foundation()
+
+
+
+		// Deals with #summary_delete, #summary_download, #summary_update
+		$('#resourceForm').on('click','#summary_delete', function() {
+			// API DELETE RESOURCE.
+			alert('Coming soon!')
+		})
+
+		$('#resourceForm').on('click','#summary_download', function() {
+			/* Currently this function is downloading client-side 
+			representation of package or ontology json.
+			*/
+			var content =  {
+				content: JSON.stringify(top.resource),
+				report_type: 'geem.json',
+				id: top.resource.metadata.prefix.toLowerCase()
+			}
+			download_data_specification(content)
+			return false
+		})
+
+		$('#resourceForm').on('click', '#summary_update', function() {
+			alert('Coming soon!')
+			return
+
+
+			var path = $('#resourceForm #summary_path').val()
+			var content =  {
+				type: 'private',
+				name: $('#resourceForm #summary_title').val(),
+				path: path
+				
+				//...
+
+			}
+
+			// USE API To send into server
+			top.resources.push(content)
+			init_resource_select(top.resources)
+			$('#specificationType').val(path)
+			//render_resource_form(data, form_URL, new_flag = false) {
+			$('#summary_delete,#summary_download,#summary_copy').show()
+			return false
+		})
+
+
+
+
+	});
 
 }
 
-function check_entity_resource(entityId) {
-	// Ensure appropriate resource is loaded for given entity id 
+function do_template_fill(template_html, data) {
+	/* Currently template has a primitive key value substitution scheme.
 
-	if (!top.resource.specifications || ! entityId in top.resource.specifications) {
+	*/
+	Object.keys(data).forEach(function(key) {
+		value = data[key]
+		// Search and replace signalled by @[variable name]
+		var re = new RegExp('@' + key ,"g");
+		template_html = template_html.replace(re, value)
+	}) 
 
-		// Ontology term prefix crudely determines resource at moment
-		var prefix = entityId.split(':')[0].toLowerCase()
+	return template_html
+}
 
-		// CURRENTLY: HARD WIRED TO JUST BE GENEPIO or FOODON
-		// If no resource loaded, or resource ontology prefix doesn't match given URL prefix ...
-		if (!top.resource.specifications || top.resource.metadata.prefix.toLowerCase() != prefix) {
-			if (prefix == 'genepio') {
-				load_resource('data/ontology/genepio-merged.json').then(check_for_hash_entity)
-				// rechecks hash
-				return false
-			}
 
-			else if (prefix == 'foodon') {
-				load_resource('data/ontology/foodon-merged.json')
-				// rechecks hash
-				return false
-			}
-		}
+
+function init_resource_select(resources) {
+	/* Populates resource selection list. Assumes resources are sorted.
+
+	*/
+	stack = resources.slice(0)
+
+	html = ['<option value="">Select a specification resource ...</option>']
+	init_resource_select_item(stack, 'ontology', html, '<optgroup label="Ontologies">')
+	init_resource_select_item(stack, 'shared', html, '</optgroup>\n<optgroup label="Shared Packages">')
+	init_resource_select_item(stack, 'private', html, '</optgroup>\n<optgroup label="My Packages (login required)">')
+
+	html.push('\n<option value="new">Add new package ...</option>\n</optgroup>')
+	html = html.join('\n')
+
+	// When a new ontology is selected:
+	$('#selectResource').html(html).on('change', do_resource_selection)
+
+}
+
+function init_resource_select_item(stack, type, html, header, manager_filter=false) {
+	/* Provide sections to display of Resource types.
+	Filter options if manager_filter supplied to just those user manages
+	which are in draft mode.
+	*/
+	html.push('\n' + header)
+	while (stack.length && stack[0].type == type && ((manager_filter && stack[0].manager && stack[0].status=='draft') || true)) {
+		html.push('\n<option value="' + stack[0].path + '">' + stack[0].name + '</option>')
+		stack.shift()
 	}
-	return true
 }
 
 
+function do_resource_selection() {
+	/* Fetch user's chosen ontology or package for display
+	*/
+	resource_URL = $('#selectResource').val()
 
-function load_resource(resource_URL) { //, resource_type
-	return new Promise(function(resolve, reject) {
-		$.ajax({
-			type: 'GET',
-			url: resource_URL,
-			timeout: 30000, //30 sec timeout
-			success: function(resource) {
-				resolve(resource);
-			},
-			error:function(XMLHttpRequest, textStatus, errorThrown) {
-				reject(Error('Given resource could not be found: \n\n\t' + resource_URL)
-				//alert('Given resource could not be found: \n\n\t' + resource_URL) 
-			}
-		});
-		
-	});
+	// This wasn't URL triggered, so clear out existing form
+	location.hash = ''
+	
+	/* Not clearing out rightside panel so that user can switch to their 
+	package after filling shopping cart, to fill it up (though this can be
+	done with shopping cart selection pulldown too).
+
+	if (top.form.formDelete) top.form.form_delete()
+	$('#resourceTabs,#content').addClass('disabled')
+	$('#shoppingCart').empty()
+	*/
+
+	if (resource_URL.length == 0) {
+		$('#resourceTabs, #content').addClass('disabled')
+		$('#specificationSourceInfoBox').show()
+		$('#tabsSpecification').hide()
+		$('#formEntityLabel').html('')
+		$('#resourceTabs').foundation('_collapseTab', $('#panelLibrary'));
+		return
+	}
+
+	if (resource_URL == 'new')
+		// User requesting to make a new package
+		resource_callback(api.get_new_resource())
+	else {
+		api.get_resource(resource_URL).then(resource_callback)
+	}
+
 
 }
-
-/*
-function load_resource(resource_URL) { //, resource_type
-	$.ajax({
-		type: 'GET',
-		url: resource_URL,
-		timeout: 30000, //30 sec timeout
-		success: function(resource) {
-
-		  top.resource = resource;
-
-		  // load_resource() triggered if hash entity id detected 
-		  // but no top.resource loaded. 
-		  check_for_hash_entity()
-		},
-		error:function(XMLHttpRequest, textStatus, errorThrown) {
-		  alert('Given resource could not be found: \n\n\t' + resource_URL) 
-		}
-	});
-}
-*/

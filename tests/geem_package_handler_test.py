3b8a23683ab9f5961732479fc5f10bcd504d0914
Ivan Gill <ivan.gill@bccdc.ca>
2019-02-19 14:36:23 -0800

    Created docker-compose.test.yml for testing purposes. Started script for testing geem_package_handler. Implemented setup and teardown of testing environment for db service to this script. Also added stub test to this script.

---
+++ tests/geem_package_handler_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+"""Tests scripts/geem_package_handler."""
+
+import subprocess
+import unittest
+
+
+class TestPackageHandling(unittest.TestCase):
+    """Test backup, delete and insert functionality."""
+
+    @classmethod
+    def setUpClass(cls):
+        # Relative path to docker-compose.test.yml
+        cls.test_yml = "../docker-compose.test.yml"
+
+        # Stop and remove default "db" service
+        subprocess.call("docker-compose down", shell=True)
+
+        # Setup test "db" service
+        run_command = "run web python /code/manage.py"
+        subprocess.call("docker-compose -f %s %s makemigrations --noinput"
+                        % (cls.test_yml, run_command), shell=True)
+        subprocess.call("docker-compose -f %s %s migrate --noinput"
+                        % (cls.test_yml, run_command), shell=True)
+        subprocess.call("docker-compose -f %s %s loaddata initial_user"
+                        % (cls.test_yml, run_command), shell=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Stop and remove test "db" service
+        subprocess.call("docker-compose -f %s down  --volumes --remove-orphans"
+                        % cls.test_yml, shell=True)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
